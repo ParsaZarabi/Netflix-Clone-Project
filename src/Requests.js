fb9ba8c924f5ee0757e968a9493f30f14e656e62
Parsa Zarabian <zarabiparsa@gmail.com>
2022-05-09 16:57:00 +0430

    first commit

---
+++ src/Requests.js
@@ -0,0 +1,11 @@
+const key = '4e120fc9e52692addd3c036c8e4e792d'
+
+const requests = {
+    requestPopular: `https://api.themoviedb.org/3/movie/popular?api_key=${key}&language=en-US&page=1`,
+    requestTopRated: `https://api.themoviedb.org/3/movie/top_rated?api_key=${key}&language=en-US&page=1`,
+    requestTrending: `https://api.themoviedb.org/3/movie/popular?api_key=${key}&language=en-US&page=2`,
+    requestHorror: `https://api.themoviedb.org/3/search/movie?api_key=${key}&language=en-US&query=horror&page=1&include_adult=false`,
+    requestUpcoming: `https://api.themoviedb.org/3/movie/upcoming?api_key=${key}&language=en-US&page=1`,
+  };
+
+  export default requests;
